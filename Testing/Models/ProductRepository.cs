670a3aca33d773a5477c0623624f821fbe22ca5a
danisylvester <maria.sylvester10@gmail.com>
2020-09-28 13:40:20 -0500

    Set up Product model with GetAllProducts method. Created a view and controller for this. Added Products to Nav bar to view a table with all bestbuy products.

---
+++ Testing/Models/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace Testing.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM Products;");
+        }
+    }
+}
